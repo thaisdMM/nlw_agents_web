d77588fdb743d3473261f2d9a5447397a25f7e9d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-07-09 13:23:59 +0200

    feat: configure React Query and fetch room data from backend

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateRoom } from './pages/create-room'
 import { Room } from './pages/room'
 
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* rota inicial que é quando acessa o app */}
-        <Route element={<CreateRoom />} index />
-        <Route element={<Room />} path="/room" />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          {/* rota inicial que é quando acessa o app */}
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,4 +1,17 @@
 // cria uma sala no create room e entra nela por esse room aqui - vão estar as perguntas e respostas...
+
+import { Navigate, useParams } from 'react-router-dom'
+
+type RoomParams = {
+  roomId: string
+}
+
 export function Room() {
-  return <div>Room Details</div>
+  const params = useParams<RoomParams>()
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />
+  }
+
+  return <div>Room Details {JSON.stringify(params)} </div>
 }
