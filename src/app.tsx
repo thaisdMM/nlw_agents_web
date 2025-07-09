10353acd850d75edf9fb19755ce5ece9746198d2
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-07-09 09:39:02 +0200

    chore: initial setup of shadcn/ui (dark mode, base button, global styles)

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,21 @@
+import { Wand } from 'lucide-react'
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <Button>Hello World</Button>
+      <Button variant="secondary">Hello World</Button>
+      <Button variant="outline">Hello World</Button>
+      <Button variant="ghost">Hello World</Button>
+      <Button variant="destructive">Hello World</Button>
+      <Button variant="link">Hello World</Button>
+      <br />
+      <Button size="sm">Hello World</Button>
+      <Button size="lg">Hello World</Button>
+      <Button size="icon">
+        <Wand />
+      </Button>
+    </div>
+  )
 }
