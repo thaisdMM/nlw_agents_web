d61d60b198f5ee2edf2742bb6d6438ed051c0aa1
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-07-11 14:18:34 +0200

    feat: finalize question creation flow and audio recording logic

--- src/http/use-create-question.ts
+++ src/http/use-create-question.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import type { CreateQuestionRequest } from './types/create-question-request'
 import type { CreateQuestionResponse } from './types/create-question-response'
+import type { GetRoomQuestionsResponse } from './types/get-room-questions-response'
 
 export function useCreateQuestion(roomId: string) {
   const queryClient = useQueryClient()
@@ -21,9 +22,72 @@ export function useCreateQuestion(roomId: string) {
 
       return result
     },
+    // parte do codigo abaixo tem haver com o "conceito de interface otimista"
+    // executa no momento que for feita a chamada para API - nesse caso: clicar em gerar nova pergunta
+    onMutate({ question }) {
+      // vai pegar todas as questions
+      const questions = queryClient.getQueryData<GetRoomQuestionsResponse>([
+        'get-questions',
+        roomId,
+      ])
 
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['get-questions', roomId] })
+      const questionsArray = questions ?? []
+
+      const newQuestion = {
+        id: crypto.randomUUID(),
+        question,
+        answer: null,
+        createdAt: new Date().toISOString(),
+        isGeneratingAnswer: true,
+      }
+
+      queryClient.setQueryData<GetRoomQuestionsResponse>(
+        ['get-questions', roomId],
+        [newQuestion, ...questionsArray]
+      )
+      return { newQuestion, questions }
     },
+
+    onSuccess(data, _variables, context) {
+      if (context?.questions) {
+        queryClient.setQueryData<GetRoomQuestionsResponse>(
+          ['get-questions', roomId],
+          (questions) => {
+            if (!questions) {
+              return questions
+            }
+
+            if (!context.newQuestion) {
+              return questions
+            }
+            return questions.map((question) => {
+              if (question.id === context.newQuestion.id) {
+                return {
+                  ...context.newQuestion,
+                  id: data.questionId,
+                  answer: data.answer,
+                  isGeneratingAnswer: false,
+                }
+              }
+              return question
+            })
+          }
+        )
+      }
+    },
+
+    // se der erro, voltar para como estava antes
+    onError(_error, _variables, context) {
+      if (context?.questions) {
+        queryClient.setQueryData<GetRoomQuestionsResponse>(
+          ['get-questions', roomId],
+          context.questions
+        )
+      }
+    },
+
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries({ queryKey: ['get-questions', roomId] })
+    // },
   })
 }
--- src/pages/record-room-audio.tsx
+++ src/pages/record-room-audio.tsx
@@ -17,12 +17,18 @@ export function RecordRoomAudio() {
   const params = useParams<RoomParams>()
   const [isRecording, setIsRecording] = useState(false)
   const recorder = useRef<MediaRecorder | null>(null)
+  const intervalRef = useRef<NodeJS.Timeout>(null)
 
+  // stop da gravação manualmente feito pelo usuário
   function stopRecording() {
     setIsRecording(false)
 
     if (recorder.current && recorder.current.state !== 'inactive') {
       recorder.current.stop()
+    }
+    // limpa o histórico para não fazer invervalRef novamente
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current)
     }
   }
   // enviar arquivos para backend
@@ -41,6 +47,30 @@ export function RecordRoomAudio() {
     console.log(result)
   }
 
+  function createRecorder(audio: MediaStream) {
+    recorder.current = new MediaRecorder(audio, {
+      //mimeType é o formato para gravar o audio
+      mimeType: 'audio/webm',
+      audioBitsPerSecond: 64_000,
+    })
+
+    recorder.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        uploadAudio(event.data)
+      }
+    }
+
+    recorder.current.onstart = () => {
+      console.log('Gravação iniciada!')
+    }
+
+    recorder.current.onstop = () => {
+      console.log('Gravaçao encerrada/pausada.')
+    }
+
+    recorder.current.start()
+  }
+
   async function startRecording() {
     if (!isRecordingSupported) {
       alert('O seu navegador não suporta gravação.')
@@ -55,23 +85,15 @@ export function RecordRoomAudio() {
         sampleRate: 44_100,
       },
     })
-    recorder.current = new MediaRecorder(audio, {
-      //mimeType é o formato para gravar o audio
-      mimeType: 'audio/webm',
-      audioBitsPerSecond: 64_000,
-    })
-    recorder.current.ondataavailable = (event) => {
-      if (event.data.size > 0) {
-        uploadAudio(event.data)
-      }
-    }
-    recorder.current.onstart = () => {
-      console.log('Gravação iniciada!')
-    }
-    recorder.current.onstop = () => {
-      console.log('Gravaçao encerrada/pausada.')
-    }
-    recorder.current.start()
+
+    createRecorder(audio)
+
+    // vai pegando o áudio em intervalos como for definido abaixo em segundos
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop()
+
+      createRecorder(audio)
+    }, 10_000)
   }
 
   if (!params.roomId) {
